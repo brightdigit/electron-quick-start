ff7a0e42e27a95a06c20792f1e39ecb1ba616217
Leo Dion <leogdion@brightdigit.com>
2017-09-05 13:59:39 -0400

    added vue

---
+++ src/js/app.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="full-page">
+  <div class="container-full">
+    <div class="row clear">
+      <div class=
+      "col-12 col-lg-6 push-lg-3 flex-center text-center pad-bottom-10">
+      <h1>Hello World!</h1>
+      </div>
+    </div>
+    <div class="row clear">
+      <div class=
+      "col-12 col-lg-6 push-lg-3 flex-center text-center">
+        <button>Signup</button> <button>Login</button>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
--- src/js/index.js
+++ src/js/index.js
@@ -44,3 +44,14 @@ var app = {
 };
 
 app.initialize();
+
+var Vue = require("vue");
+
+var App = require("./app.vue");
+
+new Vue({
+  el: "#app",
+  render: function (createElement) {
+    return createElement(App);
+  }
+});
